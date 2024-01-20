ce1861f1b01a8d09d325ad062288baf14aa09b52
Jørn V. Christensen <dev@mettle.dk>
2024-01-20 16:41:30 +0100

    Introduced RazorLight template engine. Does not quite work (does not embed text in html tags (such as head and body).
     - 404
     - Root page

--- src/webgui/app.cs
+++ src/webgui/app.cs
@@ -1,13 +1,20 @@
+using System.Text;
 using EmbedIO;
+using EmbedIO.Routing;
 using EmbedIO.WebApi;
 using LightAssistant.Interfaces;
+using RazorLight;
 
 namespace LightAssistant.WebGUI
 {
     internal class App : IDisposable, IUserInterface
     {
+        private string TemplateRoot => Path.Combine(AppContext.BaseDirectory, "templates");
+        private string TemplatePath(string name) => Path.Combine(TemplateRoot, name);
+
         private readonly IConsoleOutput _consoleOutput;
         private readonly WebServer _webServer;
+        private readonly RazorLightEngine _engine;
         private readonly string _rootUrl;
 
         public App(IConsoleOutput consoleOutput, string hostAddr, int port)
@@ -16,6 +23,10 @@ namespace LightAssistant.WebGUI
             _rootUrl = $"http://{hostAddr}:{port}";
 
             _webServer = CreateWebServer();
+            _engine = new RazorLightEngineBuilder()
+                .UseFileSystemProject(TemplateRoot)
+                .UseMemoryCachingProvider()
+                .Build();
         }
 
         public void Dispose()
@@ -34,21 +45,50 @@ namespace LightAssistant.WebGUI
             var server = new WebServer(o => o
                     .WithUrlPrefix(_rootUrl)
                     .WithMode(HttpListenerMode.EmbedIO)
-            );
-         // First, we will configure our web server by adding Modules.
-//                .WithLocalSessionManager()
-//                .WithWebApi("/api", m => m
-//                    .WithController<PeopleController>())
-//                .WithModule(new WebSocketChatModule("/chat"))
-//                .WithModule(new WebSocketTerminalModule("/terminal"))
-//                .WithStaticFolder("/", HtmlRootPath, true, m => m
-//                    .WithContentCaching(UseFileCache)) // Add static files after other modules to avoid conflicts
-//                .WithModule(new ActionModule("/", HttpVerbs.Any, ctx => ctx.SendDataAsync(new { Message = "Error" })));
+            )
+            .WithLocalSessionManager()
+            .WithWebApi("/", m => m.WithController(() => new RoutingProxy(this)));;
+
+            server.HandleHttpException(async (ctx, ex) => {
+                ctx.Response.StatusCode = ex.StatusCode;
+
+                switch (ex.StatusCode) {
+                    case 404:
+                        var text = await _engine.CompileRenderAsync(TemplatePath("404.cshtml"), model: "");
+                        await ctx.SendStringAsync(text, "text/html", Encoding.UTF8);
+                        break;
+                    default:
+                        // Handle other HTTP Status codes or call the default handler 'SendStandardHtmlAsync'
+                        await ctx.SendStandardHtmlAsync(ex.StatusCode);
+                        break;
+                }
+            });
 
             // Listen for state changes.
             server.StateChanged += (s, e) => _consoleOutput.InfoLine($"WebServer New State - {e.NewState}");
 
             return server;
+        }
+
+        public async Task<string> PageRoot()
+        {
+            var model = new { Name = "John Doe" };
+            return await _engine.CompileRenderAsync("root.cshtml", model);
+        }
+
+        
+        // Proxy class to prevent App to be disposed.
+        private class RoutingProxy : WebApiController
+        {
+            private readonly App _app;
+
+            internal RoutingProxy(App app)
+            {
+                _app = app;
+            }
+
+            [Route(HttpVerbs.Get, "/")]
+            public async Task<string> PageRoot() => await _app.PageRoot();
         }
     }
 }
