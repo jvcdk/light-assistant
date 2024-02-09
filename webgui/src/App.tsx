97bc0428f2f58293d3916c5d329bb1de6fa9faf9
Jørn Villesen Christensen <jvcdk@github.com>
2024-02-09 17:11:07 +0100

    WebGUI: Small steps... added element Device.

--- webgui/src/App.tsx
+++ webgui/src/App.tsx
@@ -1,15 +1,18 @@
 import './App.css'
+import { Device } from './Device';
 
 function MyButton() {
   return (
     <button>I'm a button</button>
   );
 }
+
 function App() {
   return (
     <div>
       <h1>Light Assistant</h1>
       <MyButton />
+      <Device name="My Device" />
     </div>
   )
 }
---
+++ webgui/src/Device.tsx
@@ -0,0 +1,7 @@
+export function Device(props: { name: string; }) {
+  return (
+    <div className='Device'>
+      A device named '{props.name}'.
+    </div>
+  );
+}
