01e6ad6bbd4e18688eeec34bafe88229e2859156
Jørn V. Christensen <dev@mettle.dk>
2024-02-10 12:13:10 +0100

    Backend: Remove RazorLight template engine. Will use separate Node.js / React framework instead.

--- src/webgui/app.cs
+++ src/webgui/app.cs
@@ -1,20 +1,12 @@
-using System.Text;
 using EmbedIO;
 using LightAssistant.Interfaces;
-using RazorLight;
 
 namespace LightAssistant.WebGUI
 {
     internal class App : IDisposable, IUserInterface
     {
-        private string TemplateRoot => Path.Combine(AppContext.BaseDirectory, "templates");
-        private string StaticRoot => Path.Combine(AppContext.BaseDirectory, "static");
-        private string TemplatePath(string name) => Path.Combine(TemplateRoot, name);
-        private string SiteName => "Light Assistant";
-
         private readonly IConsoleOutput _consoleOutput;
         private readonly WebServer _webServer;
-        private readonly RazorLightEngine _engine;
         private readonly string _rootUrl;
 
         public App(IConsoleOutput consoleOutput, string hostAddr, int port)
@@ -23,10 +15,6 @@ namespace LightAssistant.WebGUI
             _rootUrl = $"http://{hostAddr}:{port}";
 
             _webServer = CreateWebServer();
-            _engine = new RazorLightEngineBuilder()
-                .UseFileSystemProject(TemplateRoot)
-                .UseMemoryCachingProvider()
-                .Build();
         }
 
         public void Dispose()
@@ -46,53 +34,11 @@ namespace LightAssistant.WebGUI
                     .WithUrlPrefix(_rootUrl)
                     .WithMode(HttpListenerMode.EmbedIO)
             )
-            .WithLocalSessionManager()
-            .WithStaticFolder("/css/", Path.Combine(StaticRoot, "css"), isImmutable: true, m => { m.ContentCaching = true; })
-            .WithStaticFolder("/js/", Path.Combine(StaticRoot, "js"), isImmutable: true, m => { m.ContentCaching = true; })
-            .WithStaticFolder("/images/", Path.Combine(StaticRoot, "images"), isImmutable: true, m => { m.ContentCaching = true; })
-            .WithStaticFolder("/favicon.ico", Path.Combine(StaticRoot, "images/favicon.ico"), isImmutable: true, m => { m.ContentCaching = true; })
-            .WithAction("/", HttpVerbs.Any, HandleRootUrl); // Must be last as it a catch-all
-
-            server.HandleHttpException(async (ctx, ex) => {
-                ctx.Response.StatusCode = ex.StatusCode;
-
-                switch (ex.StatusCode) {
-                    case 404:
-                        var text = await _engine.CompileRenderAsync(TemplatePath("404.cshtml"), model: "");
-                        await ctx.SendStringAsync(text, "text/html", Encoding.UTF8);
-                        break;
-                    default:
-                        await ctx.SendStandardHtmlAsync(ex.StatusCode);
-                        break;
-                }
-            });
+            .WithLocalSessionManager();
 
             server.StateChanged += (s, e) => _consoleOutput.InfoLine($"WebServer New State - {e.NewState}");
 
             return server;
-        }
-
-        private async Task HandleRootUrl(IHttpContext context)
-        {
-            if(context.RequestedPath == "/") {
-                var content = await PageRoot();
-                await context.SendStringAsync(content, "text/html", Encoding.UTF8);
-            }
-            else
-                throw new HttpException(404, "Content not available.");
-        }
-
-        public async Task<string> IncludeFile(string file, object model)
-        {
-            return await _engine.CompileRenderAsync(file, model);
-        }
-
-        public async Task<string> PageRoot()
-        {
-            var model = new { 
-                SiteName,
-            };
-            return await _engine.CompileRenderAsync("root.cshtml", model);
         }
     }
 }
--- src/webgui/content-type.cs
+++
@@ -1,9 +0,0 @@
-namespace LightAssistant.WebGUI
-{
-    internal class ContentType
-    {
-        public const string TextPlain = "text/plain";
-        public const string TextHtml = "text/html";
-        public const string ApplicationJson = "application/json";
-    }
-}
