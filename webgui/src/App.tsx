74445505bde3099527055108161379c31ecc45f0
Jørn V. Christensen <dev@mettle.dk>
2024-09-29 20:52:50 +0200

    WebGUI: Introduce tabs to simplify main page a bit - and make room for the scheduler.

--- webgui/src/App.tsx
+++ webgui/src/App.tsx
@@ -1,13 +1,21 @@
+import 'react-tabs/style/react-tabs.css';
 import './App.css'
 import { DeviceList } from './DeviceList';
 import { JoinNetworkSection } from './JoinNetworkSection';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 function App() {
   return (
     <div>
       <h1>Light Assistant</h1>
-      <DeviceList />
-      <JoinNetworkSection />
+      <Tabs forceRenderTabPanel className='MainContent'>
+        <TabList>
+          <Tab>Devices</Tab>
+          <Tab>Network</Tab>
+        </TabList>
+        <TabPanel><DeviceList /></TabPanel>
+        <TabPanel><JoinNetworkSection /></TabPanel>
+      </Tabs>
     </div>
   )
 }
