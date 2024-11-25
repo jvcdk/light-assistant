441d65d629072c84e7b33de79abf23a8a6da58ce
Jørn Villesen Christensen <jvcdk@github.com>
2024-11-25 15:29:02 +0100

    WebGUI: For int and float params; add a knob that one can press and drag to set value.
    Small bug: When entering a value in one field and tabulating to next field, the whole ui is re-drawn, causing the next field to loose focus.

--- webgui/src/Utils/NumberUtils.tsx
+++ webgui/src/Utils/NumberUtils.tsx
@@ -1,8 +1,16 @@
-
 export function tryParseFloat(value: string | undefined, defaultValue: number): number {
   if (value === undefined)
     return defaultValue;
   const result = parseFloat(value);
+  if (isNaN(result))
+    return defaultValue;
+  return result;
+}
+
+export function tryParseInt(value: string | undefined, defaultValue: number): number {
+  if (value === undefined)
+    return defaultValue;
+  const result = parseInt(value);
   if (isNaN(result))
     return defaultValue;
   return result;
