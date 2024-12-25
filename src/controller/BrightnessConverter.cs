2694cdde56fb21ee242d4ffa2bc1a7bccbaa64a6
Jørn V. Christensen <dev@mettle.dk>
2024-12-25 20:05:15 +0100

    Server: Adjust gamma function to that it turns on as soon as brightness > float.epsilon.

--- src/controller/BrightnessConverter.cs
+++ src/controller/BrightnessConverter.cs
@@ -11,11 +11,29 @@ internal class BrightnessConverter
         MaxRawBrightness = maxRawBrightness;
     }
 
-    internal int NormToRaw(double normBrightness) => (int)Math.Round(ApplyGamma(normBrightness) * MaxRawBrightness);
-    internal double RawToNorm(int rawBrightness) => UnApplyGamma(rawBrightness / (double)MaxRawBrightness);
-    internal int NormToRawRaw(double normBrightness) => (int)Math.Round(normBrightness * MaxRawBrightness);
+    internal int NormToRaw(double normBrightness) {
+        if (normBrightness < MinVisibleNormBrightness)
+            return 0;
 
-    internal double MinVisibleNormBrightness => RawToNorm(1);
+        return (int)Math.Round(ApplyGamma(normBrightness) * (MaxRawBrightness - 1) + 1);
+    }
+
+    internal double RawToNorm(int rawBrightness) {
+        if (rawBrightness <= 0)
+            return 0;
+
+        var result = UnApplyGamma((rawBrightness - 1) / (double)(MaxRawBrightness - 1));
+        return Math.Clamp(result, MinVisibleNormBrightness, 1);
+    } 
+
+    internal int NormToRawRaw(double normBrightness) {
+        if (normBrightness < MinVisibleNormBrightness)
+            return 0;
+
+        return (int)Math.Round(normBrightness * (MaxRawBrightness - 1) + 1);
+    }
+
+    internal double MinVisibleNormBrightness => float.Epsilon;
 
     private double UnApplyGamma(double value) => Math.Pow(value, 1.0 / _gamma);
     private double ApplyGamma(double value) => Math.Pow(value, _gamma);
